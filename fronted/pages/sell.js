ee8d3161450412b452b4735ef532d5f9abef2d9a
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-12-10 21:00:11 -0500

    03 - intro to nextjs

---
+++ fronted/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+const Sell = props => (
+    <div>
+        <p>Hey from Sell!</p>
+        <Link href="/">
+        <a>Go to home!</a>
+      </Link>
+    </div>
+)
+export default Sell;
