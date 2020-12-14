219afc56386ca01f2f640e96d72b2bafe9774785
Mateo Velilla <mvelilla@bostonprojectllc.com>
2020-12-14 18:13:22 -0500

    05 - Intro to styled Components

--- fronted/components/Page.js
+++ fronted/components/Page.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import Meta from '../components/Meta'
+import Meta from '../components/Meta';
+import styled from 'styled-components';
+const MyButton = styled.button`
+    background: red;
+    font-size: ${props =>( props.huge ? '100px': '50px')};
+    .fox {
+        font: 100px;
+    }
+`;
+
 class Page extends Component {
     render() {
         return (
             <div>
                 <Meta/>
                 <Header/>
+                <MyButton huge>
+                    Click Me
+                    <span className="fox">🦊</span>
+                </MyButton>
                 { this.props.children}
+                <MyButton>
+                    Click Me
+                    <span className="fox">🦊</span>
+                </MyButton>
             </div>
         );
     }
