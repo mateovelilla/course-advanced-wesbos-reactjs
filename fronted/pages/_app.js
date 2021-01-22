227c837ec78e62eaa37e1ebfb9a8a3c962717b1a
Mateo velilla ospina <teovelilla@gmail.com>
2021-01-21 21:19:55 -0500

    15 - setting Up Apollo Client with React

--- fronted/pages/_app.js
+++ fronted/pages/_app.js
@@ -1,11 +1,14 @@
 import Page from '../components/Page';
-
+import { ApolloProvider } from '@apollo/client';
+import client from '../lib/client'
 function MyApp({ Component, pageProps }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={client}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
 }
 
-export default MyApp
+export default MyApp;
