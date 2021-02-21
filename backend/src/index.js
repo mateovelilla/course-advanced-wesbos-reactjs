5f3e0593b99af75bf3a12349e0b895e3e7e90b44
teovelilla <teovelilla@gmail.com>
2021-02-20 20:03:55 -0500

    24 - User Signup and Permission Flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const createServer = require('./createServer')
 const db = require('./db')
 
 const server = createServer();
+
+server.use(cookieParser())
 
 server.start(
     {
